de30cebac9027a5e825915cc48008d66aa3e11b1
RyodoTanaka <groadpg@gmail.com>
2018-08-08 13:25:21 +0900

    [update] to check forwardkinematics

--- src/urdf_example.cpp
+++ src/urdf_example.cpp
@@ -14,34 +14,21 @@ int main(int argc, char** argv)
   }
   std::ifstream ifs(argv[1]);
   std::string urdf((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
-  
+    
   auto strRobot = mc_rbdyn_urdf::rbdyn_from_urdf(urdf);
-  strRobot.mbc.q[1][0] = M_PI/4.;
-  strRobot.mbc.q[2][0] = 0;
-  strRobot.mbc.q[3][0] = -M_PI/4.;
+  
+  strRobot.mbc.q[1][0] = M_PI/3;
+  strRobot.mbc.q[2][0] = -M_PI/3;
+  strRobot.mbc.q[3][0] = -M_PI/3;
   rbd::forwardKinematics(strRobot.mb, strRobot.mbc);
 
-  auto jc = strRobot.mbc.jointConfig;
-  std::cout << jc.size() << std::endl;
-
-  for(auto itr = jc.begin(); itr!=jc.end(); itr++){
-    auto affine = sva::conversions::toAffine(*itr,
-                                             sva::conversions::LeftHanded);
-    auto homogeneous = sva::conversions::toHomogeneous(*itr,
-                                                       sva::conversions::LeftHanded);
-    // std::cout << "== Affine ==" << std::endl;
-    // std::cout << affine.translation() << std::endl;
-    // std::cout << "=================" << std::endl;
-    // std::cout << affine.rotation() << std::endl;
-
-    std::cout << "== Homogeneous ==" << std::endl;
+  // get Body position in the world frame
+  auto bpw = strRobot.mbc.bodyPosW;
+  for(auto itr = bpw.begin(); itr!=bpw.end(); itr++){
+    auto homogeneous = sva::conversions::toHomogeneous(*itr,sva::conversions::RightHanded);
+    std::cout << "== Homogeneous matrix " << itr-bpw.begin() << " in world frame ==" << std::endl;
     std::cout << homogeneous << std::endl;
-
-    
-    
   }
-
-  
   
   return 0;
 }
