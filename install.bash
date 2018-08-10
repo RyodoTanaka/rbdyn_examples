cda821e296513662f97c85af8fd4c2e5e18f567e
RyodoTanaka <groadpg@gmail.com>
2018-08-10 17:23:21 +0900

    [fix] mistaken part

--- install.bash
+++ install.bash
@@ -3,10 +3,22 @@
 SCRIPT_PATH=$( dirname ${BASH_SOURCE:-$0} )
 CURRENT=$(pwd)
 
-sudo apt install libeigen3-dev
+# Pre requirements
+sudo apt -y install git cmake pkg-config doxygen g++ libboost-dev libeigen3-dev libyaml-cpp-dev
 
-cd ${SCRIPT_PATH}/..
-git clone https://github.com/jrl-umi3218/RBDyn.git
+# SpaceVecAlg
+cd ../
+git clone --recursive https://github.com/jrl-umi3218/SpaceVecAlg.git
+cd SpaceVecAlg
+mkdir build
+cd build
+cmake -DPYTHON_BINDING=OFF ..
+make -j`nproc`
+sudo make install
+cd ../../
+
+# RBDyn
+git clone --recursive  https://github.com/jrl-umi3218/RBDyn.git
 cd RBDyn
 mkdir build
 cd build
@@ -15,8 +27,9 @@ make -j`nproc`
 sudo make install
 cd ../..
 
-git clone https://github.com/jrl-umi3218/mc_rbdyn_urdf.git
-cd mc_ubdyn_urdf
+# mc_rbdyn_urdf
+git clone --recursive https://github.com/jrl-umi3218/mc_rbdyn_urdf.git
+cd mc_rbdyn_urdf
 mkdir build
 cd build
 cmake -DPYTHON_BINDING=OFF ..
@@ -24,7 +37,10 @@ make -j`nproc`
 sudo make install
 cd ../..
 
+# Export the env
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 
-cd RBDyn_example
+# Build the rbdyn_example
+cd rbdyn_examples
 source compile.bash
+cd ${CURRENT}
