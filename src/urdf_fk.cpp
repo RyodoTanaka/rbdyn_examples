b9b713823ba3f9bb88244e560f6c843c525c9779
RyodoTanaka <groadpg@gmail.com>
2018-08-10 10:40:29 +0900

    [update] add comment

--- src/urdf_fk.cpp
+++ src/urdf_fk.cpp
@@ -3,13 +3,13 @@
 #include <SpaceVecAlg/Conversions.h>
 #include <Eigen/Core>
 
-
 #include <iostream>
 #include <fstream>
 #include <cmath>
 
 int main(int argc, char** argv)
 {
+  // Load the URDF file
   std::string filename;
   if(argc!=1)
     filename = argv[1];
@@ -18,12 +18,16 @@ int main(int argc, char** argv)
 
   std::ifstream ifs(filename);
   std::string urdf((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
-    
+
+  // set ParsedResult from URDF file
   auto strRobot = mc_rbdyn_urdf::rbdyn_from_urdf(urdf);
-  
+
+  // set values for each joints
   strRobot.mbc.q[1][0] = M_PI/3;
   strRobot.mbc.q[2][0] = -M_PI/3;
   strRobot.mbc.q[3][0] = -M_PI/3;
+
+  // Solve the FK
   rbd::forwardKinematics(strRobot.mb, strRobot.mbc);
 
   // get Body position in the world frame
