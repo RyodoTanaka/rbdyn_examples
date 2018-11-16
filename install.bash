ecf59f55317748bf1cfaa10f79b8d4c99c472ccf
RyodoTanaka <groadpg@gmail.com>
2018-11-16 18:04:44 +0900

    [update] installation script

--- install.bash
+++ install.bash
@@ -4,7 +4,18 @@ SCRIPT_PATH=$( dirname ${BASH_SOURCE:-$0} )
 CURRENT=$(pwd)
 
 # Pre requirements
-sudo apt -y install git cmake pkg-config doxygen g++ libboost-dev libeigen3-dev libyaml-cpp-dev
+sudo apt-get install git cmake pkg-config doxygen g++ libboost1.58-dev libboost1.58-all-dev libeigen3-dev libyaml-cpp-dev python-pip python2.7-dev make libtinyxml2-dev -y
+
+# Eigen3ToPython
+cd ../
+git clone https://github.com/jrl-umi3218/Eigen3ToPython.git
+cd Eigen3ToPython
+mkdir build
+cd build
+cmake .. -DPYTHON_BINDING_USER_INSTALL=ON
+make -j`nproc`
+sudo make install
+cd ../..
 
 # SpaceVecAlg
 cd ../
@@ -12,17 +23,17 @@ git clone --recursive https://github.com/jrl-umi3218/SpaceVecAlg.git
 cd SpaceVecAlg
 mkdir build
 cd build
-cmake -DPYTHON_BINDING=OFF ..
+cmake .. -DPYTHON_BINDING=ON -DPYTHON_BINDING_USER_INSTALL=ON
 make -j`nproc`
 sudo make install
-cd ../../
+cd ../..
 
 # RBDyn
 git clone --recursive  https://github.com/jrl-umi3218/RBDyn.git
 cd RBDyn
 mkdir build
 cd build
-cmake -DPYTHON_BINDING=OFF ..
+cmake .. -DPYTHON_BINDING=ON -DPYTHON_BINDING_USER_INSTALL_DEFAULT=ON -DPYTHON_BINDING_USER_INSTALL=ON
 make -j`nproc`
 sudo make install
 cd ../..
@@ -32,7 +43,7 @@ git clone --recursive https://github.com/jrl-umi3218/mc_rbdyn_urdf.git
 cd mc_rbdyn_urdf
 mkdir build
 cd build
-cmake -DPYTHON_BINDING=OFF ..
+cmake .. -DPYTHON_BINDING=ON -DPYTHON_BINDING_USER_INSTALL=ON
 make -j`nproc`
 sudo make install
 cd ../..
@@ -41,6 +52,6 @@ cd ../..
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 
 # Build the rbdyn_example
-cd rbdyn_examples
+cd ${CURRENT}
 source compile.bash
 cd ${CURRENT}
